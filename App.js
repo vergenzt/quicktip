b47680d2b10c1f233229a5c4481a2fd5592b39e3
Tim Vergenz <vergenzt@gmail.com>
2018-06-12 15:04:25 -0700

    Initial working commit!

---
+++ App.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { createStackNavigator } from 'react-navigation';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+  }
+});
+
+export class MainScreen extends React.Component {
+  static navigationOptions = ({navigation: {navigate}}) => ({
+    title: 'QuickTip',
+    headerRight: <Button title='⋮' onPress={() => navigate('Settings')}/>
+  });
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Yay!</Text>
+      </View>
+    );
+  }
+}
+
+export class SettingsScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Settings'
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Some settings</Text>
+      </View>
+    );
+  }
+}
+
+export default createStackNavigator({
+  Main: MainScreen,
+  Settings: SettingsScreen
+}, {
+  initialRouteName: 'Main'
+});
